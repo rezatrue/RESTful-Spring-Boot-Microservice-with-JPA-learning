d0ac36c5e39c6323912c813a7605a3dfd3c5f9bd
rezatrue <rezatrue@gmail.com>
2017-10-11 05:13:13 +0600

    import spting boot init

---
+++ explorecli/src/main/java/com/example/ec/explorecli/ExplorecliApplication.java
@@ -0,0 +1,12 @@
+package com.example.ec.explorecli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecliApplication.class, args);
+	}
+}
