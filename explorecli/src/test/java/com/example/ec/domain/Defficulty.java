2d8cfab2214397bad93f17ac4225bf9b4a603b5a
rezatrue <rezatrue@gmail.com>
2017-10-17 06:03:55 +0600

    Lession Chapter 2

--- explorecli/src/test/java/com/example/ec/domain/Defficulty.java
+++
@@ -1,5 +0,0 @@
-package com.example.ec.domain;
-
-public enum Defficulty {
-	Easy, Medium, Difficult, Varies;
-}
--- explorecli/src/test/java/com/example/ec/repo/TourPackageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ec.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.ec.domain.TourPackage;
-
-public interface TourPackageRepository extends CrudRepository<TourPackage, String>{
-
-}
